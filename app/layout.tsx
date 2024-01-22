d22d32737826a39681d186a0f7df1a115c0cf42a
laiane langa <112904042+lailanga@users.noreply.github.com>
2024-01-22 17:22:03 -0400

    theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
+import StyledJsxRegistry from "./registry";
+
 export default function RootLayout({
     children,
   }: {
     children: React.ReactNode
   }) {
     return (
-      <html lang="pt-br">
-        <body>{children}</body>
+      <html lang="pt-BR">
+        <body>
+          <StyledJsxRegistry>{children}</StyledJsxRegistry>
+        </body>
       </html>
     )
   }
