f7e1c6d93e957435f22c96b2f1c686b76774426e
laiane langa <112904042+lailanga@users.noreply.github.com>
2024-01-21 22:19:34 -0400

    update

--- core/crud.ts
+++ core/crud.ts
@@ -1,16 +1,93 @@
 import fs from "fs"; //ES6
+import { v4 as uuid } from 'uuid';
 
 // const fs = require("fs");
 const DB_FILE_PATH = "./core/db"
 
 console.log("[CRUD]");
 
-function create(content: string) {
-    fs.writeFileSync(DB_FILE_PATH, content)
-    return content;
+interface Todo {
+    id: string;
+    date: string;
+    content: string;
+    done: boolean;
+}
+
+function create(content: string): Todo {
+    const todo: Todo = {
+        id: uuid(),
+        date: new Date().toISOString(),
+        content: content,
+        done: false,
+    };
+    //console.log(todo);
+
+    const todos: Array<Todo> = [
+        ...read(),
+        todo,
+    ];
+
+    // salvar o content no sistema
+    //fs.writeFileSync(DB_FILE_PATH, content);
+    fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
+        todos,
+    }, null, 2));
+
+    //return content;
+    return todo;
+}
+
+function read(): Array<Todo> {
+    const dbString = fs.readFileSync(DB_FILE_PATH, "utf-8");
+    const db = JSON.parse(dbString || "{}");
+    if(!db.todos){
+        return [];
+    }
+    return db.todos;
+}
+
+// update(DEQUEM, OQUE);
+function update(id: string, partialTodo: Partial<Todo>): Todo {
+    let updateTodo;
+    const todos = read();
+    todos.forEach((currentTodo) => {
+        const isToUpdate = currentTodo.id === id;
+        if(isToUpdate) {
+            updateTodo = Object.assign(currentTodo, partialTodo);
+        }
+    });
+    //console.log("TODOS atualizadas >> ", todos);
+    fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
+        todos,
+    }, null, 2));
+    
+    if(!updateTodo) {
+        throw new Error("Please, provide another ID!")
+    }
+
+    return updateTodo;
+}
+
+function updateContentById(id: string, content: string): Todo {
+    return update(id, {
+        content,
+    });
+}
+
+function CLEAR_DB() {
+    fs.writeFileSync(DB_FILE_PATH, "");
 }
 
 
 // [SIMULATION]
-console.log(create("Hoje é domingo"));
+CLEAR_DB();
+create("Primeira TODO");
+create("Segunda TODO");
+const terceiraTodo = create("Segunda TODO");
+// update(terceiraTodo.id, {
+//     content: "Terceira Todo ok",
+//     done: true
+// });
+updateContentById(terceiraTodo.id, "Atualizada Terceira");
+console.log(read());
 
