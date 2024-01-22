8451c668bc0b2df9c51204303c60c44419d9289b
laiane langa <112904042+lailanga@users.noreply.github.com>
2024-01-21 20:15:51 -0400

    crud com qualidade

---
+++ core/crud.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const DB_FILE_PATH = "./core/db"
+
+console.log("[CRUD]");
+
+function create(content) {
+    fs.writeFileSync(DB_FILE_PATH, content)
+    return content;
+}
+
+
+// [SIMULATION]
+console.log(create("Hoje é domingo pe de caximbo"));
+
